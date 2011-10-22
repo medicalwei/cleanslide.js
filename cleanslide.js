51a1824337f7ab8e93418ce976c69c97d15b89f9
Yao Wei (魏銘廷) <mwei@lxde.org>
2011-10-22 11:42:25 +0800

    added some comments. remove console.log.

--- cleanslide.js
+++ cleanslide.js
@@ -5,7 +5,7 @@ $(window).ready(function(){
     $("body>*").removeClass("current");
     this.addClass("current");
     var innerOffset;
-    if (this.height() <= $(window).height()) {
+    if (this.height() <= $(window).height()) { // check if it is a big block
       innerOffset = (this.outerHeight() - $(window).height()) / 2;
     } else {
       innerOffset = (this.outerHeight()-this.height()) / 2 + $(window).height() * subpage * 0.8; // overflow
@@ -19,28 +19,22 @@ $(window).ready(function(){
   });
 
   $(window).keyup(function(event){
-    console.log(event);
     var blocks = $('body>*');
     var current = blocks.eq(page);
+
     if (event.keyCode == 40) { // down
       innerBottom = current.position().top + (current.outerHeight()-current.height()) / 2 + current.height();
       console.log(innerBottom);
-      if (innerBottom > $(window).height())
-      {
+      if (innerBottom > $(window).height()) { // if the block does not reach the screen bottom
         subpage += 1;
-      }
-      else if (page < (blocks.size()-1))
-      {
+      } else if (page < (blocks.size()-1)) {
         page += 1;
         subpage = 0;
       }
     } else if (event.keyCode == 38) { // up
-      if (subpage > 0)
-      {
+      if (subpage > 0) {
         subpage -= 1;
-      }
-      else if (page > 0)
-      {
+      } else if (page > 0) {
         page -= 1;
         subpage = 0;
       }
