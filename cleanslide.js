eeff7aa726a760a438a2222773263bf809d7a9ce
Yao Wei (魏銘廷) <mwei@lxde.org>
2011-11-02 10:28:55 +0800

    add more keybindings

--- cleanslide.js
+++ cleanslide.js
@@ -1,5 +1,7 @@
-page = 0;
-subpage = 0;
+var page = 0;
+var subpage = 0;
+var blocks;
+
 $(document).ready(function(){
   blocks = $('body>*');
 
@@ -33,33 +35,16 @@ $(document).ready(function(){
   });
 
   $(window).keyup(function(event){
-    var current = blocks.eq(page);
-
     switch (event.keyCode)
     {
-      case 40:
-        innerBottom = current.position().top + (current.outerHeight()-current.height()) / 2 + current.height();
-        console.log(innerBottom);
-        if (innerBottom > $(window).height()) { // if the block does not reach the screen bottom
-          subpage += 1;
-        } else if (page < (blocks.size()-1)) {
-          page += 1;
-          subpage = 0;
-        }
-        break;
-
-      case 38:
-        if (subpage > 0) {
-          subpage -= 1;
-        } else if (page > 0) {
-          page -= 1;
-          subpage = 0;
-        }
-        break;
+      case 38: case 37: pageUp(false); break; // up, left
+      case 32: case 40: case 39: pageDown(false); break; // down, right, space
+      case 33: pageUp(true); break; // PgUp
+      case 34: pageDown(true); break; // PgDn
+      case 36: goHome(); break; // Home
+      case 35: goToEnd(); break; // End
     }
-
-    blocks.eq(page).setToCenter(subpage);
-    window.location.hash = "#" + page;
+    console.log(event.keyCode);
   });
 
   // get hash
@@ -71,3 +56,41 @@ $(document).ready(function(){
   blocks.eq(page).setToCenter(subpage);
 });
 
+pageDown = function(toNextSection) {
+  var current = blocks.eq(page);
+
+  innerBottom = current.position().top + (current.outerHeight()-current.height()) / 2 + current.height();
+  if (!toNextSection && innerBottom > $(window).height()) { // if the block does not reach the screen bottom
+    subpage += 1;
+  } else if (page < (blocks.size()-1)) {
+    page += 1;
+    subpage = 0;
+  }
+  blocks.eq(page).setToCenter(subpage);
+  window.location.hash = "#" + page;
+}
+
+pageUp = function(toPrevSection) {
+  if (!toPrevSection && subpage > 0) {
+    subpage -= 1;
+  } else if (page > 0) {
+    page -= 1;
+    subpage = 0;
+  }
+  blocks.eq(page).setToCenter(subpage);
+  window.location.hash = "#" + page;
+}
+
+goHome = function() {
+  page = 0;
+  subpage = 0;
+  blocks.eq(page).setToCenter(subpage);
+  window.location.hash = "#" + page;
+}
+
+goToEnd = function() {
+  page = blocks.size()-1;
+  subpage = 0;
+  blocks.eq(page).setToCenter(subpage);
+  window.location.hash = "#" + page;
+}
